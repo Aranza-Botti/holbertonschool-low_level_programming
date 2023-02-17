b9404f33c58df083e65a1bb9bded7239b8c83e57
Aranza <aranza.botti@gmail.com>
2023-02-17 12:00:19 -0800

    101

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -8,7 +8,8 @@
 */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message =
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 ssize_t message_len = 59;
 write(STDERR_FILENO, message, message_len);
 return (1);
