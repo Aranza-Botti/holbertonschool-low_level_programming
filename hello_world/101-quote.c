5c324995e78b020377e8901c8ac2634676381a8a
Aranza <aranza.botti@gmail.com>
2023-02-17 11:53:45 -0800

    101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<unistd.h>
+/**
+* main - Entry point
+*
+* Description: Prints a message to standard error
+*
+* Return: Always 1
+*/
+int main(void)
+{
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t message_len = 59;
+write(STDERR_FILENO, message, message_len);
+return (1);
+}
