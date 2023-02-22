eaaacb84db9b5c50df15cdc12becd77f47bd1357
Aranza <aranza.botti@gmail.com>
2023-02-21 19:21:12 -0800

    listo

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+* main - Prints the alphabet in lowercase, then in uppercase.
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+char d = 'A';
+while (d <= 'Z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
