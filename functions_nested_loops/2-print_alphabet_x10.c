bf9c02c3a86af573552aa1b9b1f61d021c2fc9d3
Aranza <aranza.botti@gmail.com>
2023-02-23 12:43:52 -0800

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - alphabet 10 times.
+*
+* Return: Nothing.
+*
+*/
+void print_alphabet_x10(void)
+{
+int printx10;
+char alphabet;
+for (printx10 = 0; printx10 < 10; ++printx10)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
