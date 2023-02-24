c65bc5be98f1e612ce91d4ad9572055a7f187813
Aranza <aranza.botti@gmail.com>
2023-02-24 08:46:57 -0800

    7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_diagonal - diagonal line on the terminal
+* @n: Amount of \ to be printed
+*/
+void print_diagonal(int n)
+{
+int a, b;
+if (n <= 0)
+_putchar('\n');
+else
+for (b = 0; n > 0; n--, b++)
+{
+for (a = 0; i < j; a++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
